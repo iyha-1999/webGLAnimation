049ceca1a9e59c87534c07d209ca94c3a54a2eac
iyha-1999 <iyha1999end@gmail.com>
2022-06-28 09:51:53 +0900

    不要なLogoファイルを削除

--- js/unit/logo/view/view_renderer.js
+++
@@ -1,9 +0,0 @@
-var viewRenderer = {
-  webGLRenderer: new THREE.WebGLRenderer(),
-  set: function(){
-    this.webGLRenderer.setClearColor(new THREE.Color(0xEEEEEE));
-    this.webGLRenderer.setSize(window.innerWidth, window.innerHeight);
-    document.getElementById("WebGL-output").appendChild(this.webGLRenderer.domElement);
-    return this.webGLRenderer;
-  }
-};
